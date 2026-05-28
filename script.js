4e1be4bbc3d5e8aece358a8afd2619ca00969bde
riplool <aniruddhswaraj@gmail.com>
2026-05-28 19:37:12 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,17 @@
+function showPage(pageId) {
+  const pages = document.querySelectorAll('.page');
+
+  pages.forEach(page => {
+    page.classList.remove('active');
+  });
+
+  document.getElementById(pageId).classList.add('active');
+}
+
 function copyIP() {
-  const ip =
-    document.getElementById('serverIP').innerText;
+  const ip = document.getElementById("ip").innerText;
 
   navigator.clipboard.writeText(ip);
 
-  const button =
-    document.querySelector('.copy-btn');
-
-  button.innerText = 'Copied!';
-
-  setTimeout(() => {
-    button.innerText = 'Copy IP';
-  }, 2000);
+  alert("IP Copied!");
 }
-
-function showPage(pageId) {
-  const pages =
-    document.querySelectorAll('.page');
-
-  pages.forEach(page => {
-    page.classList.remove('active-page');
-  });
-
-  document
-    .getElementById(pageId)
-    .classList.add('active-page');
-
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  });
-}
-
-window.addEventListener('scroll', () => {
-  const navbar =
-    document.querySelector('.navbar');
-
-  if (window.scrollY > 50) {
-    navbar.style.background =
-      'rgba(0,0,0,.82)';
-  } else {
-    navbar.style.background =
-      'rgba(0,0,0,.55)';
-  }
-});
